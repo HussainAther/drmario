9ed1a47d1997f36b8115cbaf0820b206bd08763d
Syed Hussain Ather <shussainather@gmail.com>
2019-12-28 00:51:29 -0800

    width and height

--- board.py
+++ board.py
@@ -5,3 +5,7 @@ import random
 Create the board. We want to use the blocks to fill up the board
 with a starting setup and allow blocks to fall as they do.
 """
+
+width = 10
+height = 20
+widthpixels = width*drpython.block.width
