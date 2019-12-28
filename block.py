9719afc79f6fcd5d48fad4571cce0d7d60411706
Syed Hussain Ather <shussainather@gmail.com>
2019-12-27 21:41:42 -0800

    class blcok

--- block.py
+++ block.py
@@ -1,5 +1,15 @@
 class Color(object):
+    """
+    Give the object a color depending on how it spawns
+    and how it touches another block.
+    """
     clear = 0
     blue = 1
     red = 2
     yellow = 3
+
+class Block(object):
+    """
+    Give properties to the block itself. It needs to fall
+    at a certain speed and keep track of its color.
+    """
