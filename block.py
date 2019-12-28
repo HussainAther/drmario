4e982c11f2c2b22735710887110fa31d04610be2
Syed Hussain Ather <shussainather@gmail.com>
2019-12-27 21:47:24 -0800

    initialize

--- block.py
+++ block.py
@@ -13,3 +13,13 @@ class Block(object):
     Give properties to the block itself. It needs to fall
     at a certain speed and keep track of its color.
     """
+    def __init__(self, x, y, color=None):
+        """
+        Initialize a cleared empty block.
+        """
+        self.x = x
+        self.y = y
+        self.color = Color.CLEAR
+        self.falling = False
+        if color:
+            self.setcolor(color)
