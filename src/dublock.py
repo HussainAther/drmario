62512aac4c1510d130777152e3e23baf7b979348
Syed Hussain Ather <shussainather@gmail.com>
2019-12-30 23:09:43 -0800

    orient

--- src/dublock.py
+++ src/dublock.py
@@ -5,7 +5,25 @@ class dublock(object):
     """
     def __init__(self, blocks):
         self._blocks = blocks
-        
+        self._images = None 
+
+    def getorient(self):
+        """
+        Get the orientation (from the list o) of each block in the dublock (megavitamin).
+        This is used for sprite purposes.
+        """
+        a, b = self.blocks # Get the two individuals blocks of the 
+                                   # dublock.
+        if a.y < b.y:
+            o = ["bottom", "top"]
+        elif a.y > b.y:
+            o = ["top", "bottom"]
+        elif a.x < b.x:
+            o = ["left", "right"] 
+        elif a.x > b.x:
+            o =  ["right", "left"]
+        return o
+
     def setblocks(self, blocka, blockb):
         self._blocks = (blocka, blockb)
 
